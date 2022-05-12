0321d06c61d705295763a75da49ddb54adfcef17
masafumimori <m.masafumi@outlook.com>
2022-05-12 11:43:57 +0900

    fix: install package and modify files

--- web3.0/client/src/utils/constants.ts
+++ web3.0/client/src/utils/constants.ts
@@ -1,4 +1,4 @@
 import abi from "./Transactions.json";
 
-export const contractAddress = "0xfCCF80344a668b72ac4Be23513F0E9E4a35C84fA";
+export const contractAddress = "0x3bBf42082704CB545875f9B14B29e0332e3BfDDd";
 export const contractABI = abi.abi;
