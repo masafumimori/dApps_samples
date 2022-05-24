3d9ba457543ac107dd2932c2b66934b85e39eb8b
masafumimori <m.masafumi@outlook.com>
2022-05-24 14:03:14 +0900

    setup moralis

--- nft-dashboard/src/App.tsx
+++ nft-dashboard/src/App.tsx
@@ -1,45 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from 'react';
+import { useMoralis } from 'react-moralis';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const { authenticate, isAuthenticated, user } = useMoralis();
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+	if (!isAuthenticated) {
+		return (
+			<div>
+				<button onClick={() => authenticate()}>Authenticate</button>
+			</div>
+		);
+	}
 
-export default App
+	return (
+		<div>
+			<h1>Welcome {user?.get('username')}</h1>
+		</div>
+	);
+};
+
+export default App;
--- nft-dashboard/src/main.tsx
+++ nft-dashboard/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { MoralisProvider } from 'react-moralis';
+import App from './App';
+
+const MORALIS_SERVER_URL = import.meta.env.VITE_MORALIS_SERVER_URL;
+const MORALIS_APP_ID = import.meta.env.VITE_MORALIS_APP_ID;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+	<React.StrictMode>
+		<MoralisProvider serverUrl={MORALIS_SERVER_URL} appId={MORALIS_APP_ID}>
+			<App />
+		</MoralisProvider>
+	</React.StrictMode>
+);
