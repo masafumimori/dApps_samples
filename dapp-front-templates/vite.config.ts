1c6907587a342b54262c2fb6b7b00e91b2b9e756
masafumimori <m.masafumi@outlook.com>
2022-05-31 09:02:02 +0900

    add path alias

--- dapp-front-templates/vite.config.ts
+++ dapp-front-templates/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
+      '@/': path.join(__dirname, 'src/'),
       process: 'process/browser',
       stream: 'stream-browserify',
       zlib: 'browserify-zlib',
